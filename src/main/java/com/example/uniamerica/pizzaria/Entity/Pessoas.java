19db733c8fc5dc103d1722c8ef9174f94495e40b
vainicius <comercial.viniciusgabriel@gmail.com>
2023-08-13 16:08:22 -0300

    Pessoas - Entidades OK!

--- src/main/java/com/example/uniamerica/pizzaria/Entity/Pessoas.java
+++ src/main/java/com/example/uniamerica/pizzaria/Entity/Pessoas.java
@@ -1,15 +1,29 @@
 package com.example.uniamerica.pizzaria.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Table(name="pessoas_table", schema = "public")
 public class Pessoas {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @NotNull @Column(name="id", unique = true)
     private Long id;
 
+    @Getter @Setter
+    @NotNull @Size(min = 2, max = 40)
+    @Column(name="nome", length = 40, nullable = false)
+    private String nome;
 
+    @Getter @Setter
+    @NotNull @Column(name="cpf", nullable = false, unique = true, length = 15)
+    private String cpf;
+
+    @Getter @Setter
+    @Column(name="telefone", length = 20)
+    private String telefone;
 }
