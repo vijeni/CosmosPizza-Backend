875daa08dce7b74c58c7ba3951bec367501f2357
vainicius <comercial.viniciusgabriel@gmail.com>
2023-08-13 15:55:13 -0300

    Estrutura básica Spring iniciada

---
+++ src/test/java/com/example/uniamerica/pizzaria/PizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniamerica.pizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
