491612ea83e8bda93b36783dbeea5f33fd1173be
vainicius <comercial.viniciusgabriel@gmail.com>
2023-08-15 15:03:52 -0300

    PessoaController, PessoaService - Post Ok!

--- src/main/java/com/example/uniamerica/pizzaria/Controller/PessoaController.java
+++ src/main/java/com/example/uniamerica/pizzaria/Controller/PessoaController.java
@@ -1,18 +1,19 @@
 package com.example.uniamerica.pizzaria.Controller;
 
+import com.example.uniamerica.pizzaria.DTO.PessoaDTO;
 import com.example.uniamerica.pizzaria.Repository.PessoaRepository;
+import com.example.uniamerica.pizzaria.Service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/pessoas")
 public class PessoaController {
     @Autowired
     PessoaRepository repository;
+    @Autowired
+    PessoaService service;
     @GetMapping("/id")
     public ResponseEntity<?>findById(@RequestParam("id") final Long id){
         try{
@@ -21,8 +22,14 @@ public class PessoaController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
-
-
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?>cadastrar(@RequestBody PessoaDTO pessoa){
+        try{
+            return ResponseEntity.ok(service.post(pessoa));
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
 
 }
