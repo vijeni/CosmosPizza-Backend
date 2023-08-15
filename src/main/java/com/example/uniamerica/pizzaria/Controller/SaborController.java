2cc36a8d165cc062582a7d2dde7c30f0fc9ff55f
vainicius <comercial.viniciusgabriel@gmail.com>
2023-08-15 16:42:32 -0300

    SaborController, SaborRepository - Cadastrar OK!

--- src/main/java/com/example/uniamerica/pizzaria/Controller/SaborController.java
+++ src/main/java/com/example/uniamerica/pizzaria/Controller/SaborController.java
@@ -1,20 +1,21 @@
 package com.example.uniamerica.pizzaria.Controller;
 
+import com.example.uniamerica.pizzaria.DTO.SaborDTO;
 import com.example.uniamerica.pizzaria.Repository.SaborRepository;
+import com.example.uniamerica.pizzaria.Service.SaborService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value="/api/sabores")
 public class SaborController {
     @Autowired
     SaborRepository repository;
+    @Autowired
+    SaborService service;
 
-    @GetMapping("id")
+    @GetMapping("/id")
     public ResponseEntity<?>findById(@RequestParam("id") final long id){
         try{
            return ResponseEntity.ok(repository.findById(id).orElse(null));
@@ -23,7 +24,14 @@ public class SaborController {
         }
     }
 
-
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?>cadastrar(@RequestBody SaborDTO sabor){
+        try{
+            return ResponseEntity.ok(service.cadastrar(sabor));
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
 
 }
---
+++ src/main/java/com/example/uniamerica/pizzaria/Service/SaborService.java
@@ -0,0 +1,45 @@
+package com.example.uniamerica.pizzaria.Service;
+
+import com.example.uniamerica.pizzaria.DTO.SaborDTO;
+import com.example.uniamerica.pizzaria.Entity.Sabor;
+import com.example.uniamerica.pizzaria.Repository.SaborRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+@Service
+public class SaborService {
+    @Autowired
+    private SaborRepository repository;
+
+    public Sabor toSaborEntidade(SaborDTO saborDTO){
+        Sabor sabor = new Sabor();
+        sabor.setDescricao(saborDTO.getDescricao());
+        sabor.setNome(saborDTO.getNome());
+        sabor.setIngredientes(saborDTO.getIngredientes());
+
+
+        return sabor;
+    }
+
+    public SaborDTO toSaborDTO (Sabor saborEntidade){
+
+       SaborDTO saborDTO = new SaborDTO();
+       saborDTO.setDescricao(saborEntidade.getDescricao());
+       saborDTO.setNome(saborEntidade.getNome());
+       saborDTO.setIngredientes(saborEntidade.getIngredientes());
+       saborDTO.setId(saborEntidade.getId());
+        return  saborDTO;
+    }
+
+
+
+    public SaborDTO cadastrar(SaborDTO sabor){
+        Assert.notNull(sabor.getIngredientes(),"Por favor, diga quais são os ingredientes!");
+        Assert.notNull(sabor.getNome(),"Por favor, digite um nome para o sabor!");
+
+        return toSaborDTO(repository.save(toSaborEntidade(sabor)));
+    }
+
+
+}
