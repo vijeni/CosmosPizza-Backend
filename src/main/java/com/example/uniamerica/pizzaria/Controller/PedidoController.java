2f6d1eab73582c34e681a97777dd20547d301c1b
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-08-17 14:49:13 -0300

    Padronização nas rotas para substantivo singular

--- src/main/java/com/example/uniamerica/pizzaria/Controller/PedidoController.java
+++ src/main/java/com/example/uniamerica/pizzaria/Controller/PedidoController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/pedidos")
+@RequestMapping("/api/pedido")
 public class PedidoController {
 }
--- src/main/java/com/example/uniamerica/pizzaria/Controller/PizzaController.java
+++ src/main/java/com/example/uniamerica/pizzaria/Controller/PizzaController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/pizzas")
+@RequestMapping("/api/pizza")
 public class PizzaController {
     @Autowired
     PizzaService service;
