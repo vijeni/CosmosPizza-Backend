5adddf254c7f566a96d62037789ad4ac11513d64
vainicius <comercial.viniciusgabriel@gmail.com>
2023-08-16 21:42:34 -0300

    Alterando vários ModelMapper's

--- src/main/java/com/example/uniamerica/pizzaria/Service/EnderecosService.java
+++ src/main/java/com/example/uniamerica/pizzaria/Service/EnderecosService.java
@@ -1,9 +1,12 @@
 package com.example.uniamerica.pizzaria.Service;
 
 import com.example.uniamerica.pizzaria.DTO.EnderecoDTO;
+import com.example.uniamerica.pizzaria.DTO.PessoaDTO;
 import com.example.uniamerica.pizzaria.Entity.Endereco;
+import com.example.uniamerica.pizzaria.Entity.Pessoa;
 import com.example.uniamerica.pizzaria.Repository.EnderecoRepository;
 import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
@@ -14,24 +17,24 @@ public class EnderecosService {
 
     @Autowired
     private EnderecoRepository repository;
+
+    private ModelMapper modelMapper = new ModelMapper();
+
+    public Pessoa toPessoa(PessoaDTO pessoaDTO){
+        return modelMapper.map(pessoaDTO,Pessoa.class);
+    }
+
+    public PessoaDTO toPessoaDTO(Pessoa pessoaEntidade){
+        return modelMapper.map(pessoaEntidade, PessoaDTO.class);
+    }
+
     public Endereco toEnderecos (EnderecoDTO enderecoDTO){
-        Endereco endereco = new Endereco();
-        endereco.setBairro(enderecoDTO.getBairro());
-        endereco.setCep(enderecoDTO.getCep());
-        endereco.setComplemento(enderecoDTO.getComplemento());
-        endereco.setNumero(enderecoDTO.getNumero());
-        endereco.setLogradouro(enderecoDTO.getLogradouro());
-        return endereco;
+        return modelMapper.map(enderecoDTO,Endereco.class);
     }
 
     public EnderecoDTO toEnderecosDTO (Endereco endereco){
-        EnderecoDTO enderecoDTO = new EnderecoDTO();
-        enderecoDTO.setBairro(endereco.getBairro());
-        enderecoDTO.setCep(endereco.getCep());
-        enderecoDTO.setComplemento(endereco.getComplemento());
-        enderecoDTO.setNumero(endereco.getNumero());
-        enderecoDTO.setLogradouro(endereco.getLogradouro());
-        return enderecoDTO;
+
+        return modelMapper.map(endereco,EnderecoDTO.class);
     }
 
     @Transactional
@@ -40,7 +43,6 @@ public class EnderecosService {
         Assert.notNull(enderecoDTO.getCep(),"Por favor, informe um CEP!");
         Assert.hasText(enderecoDTO.getBairro(),"Informe um bairro válido!");
         Assert.hasText(enderecoDTO.getCep(),"Informe um CEP válido!");
-
 
         return toEnderecosDTO(repository.save(toEnderecos(enderecoDTO)));
     }
--- src/main/java/com/example/uniamerica/pizzaria/Service/SaborService.java
+++ src/main/java/com/example/uniamerica/pizzaria/Service/SaborService.java
@@ -1,6 +1,8 @@
 package com.example.uniamerica.pizzaria.Service;
 
+import com.example.uniamerica.pizzaria.DTO.PessoaDTO;
 import com.example.uniamerica.pizzaria.DTO.SaborDTO;
+import com.example.uniamerica.pizzaria.Entity.Pessoa;
 import com.example.uniamerica.pizzaria.Entity.Sabor;
 import com.example.uniamerica.pizzaria.Repository.SaborRepository;
 import com.fasterxml.jackson.databind.jsontype.impl.AsExistingPropertyTypeSerializer;
@@ -15,43 +17,32 @@ public class SaborService {
     private SaborRepository repository;
     private ModelMapper modelMapper = new ModelMapper();
 
-    public Sabor toSaborEntidade(SaborDTO saborDTO){
-        Sabor sabor = new Sabor();
-        sabor.setDescricao(saborDTO.getDescricao());
-        sabor.setNome(saborDTO.getNome());
-        sabor.setIngredientes(saborDTO.getIngredientes());
-
-
-        return sabor;
+    public Sabor toSabor(SaborDTO saborDTO){
+        return modelMapper.map(saborDTO,Sabor.class);
     }
 
-    public SaborDTO toSaborDTO (Sabor saborEntidade){
-
-       SaborDTO saborDTO = new SaborDTO();
-       saborDTO.setDescricao(saborEntidade.getDescricao());
-       saborDTO.setNome(saborEntidade.getNome());
-       saborDTO.setIngredientes(saborEntidade.getIngredientes());
-       saborDTO.setId(saborEntidade.getId());
-        return  saborDTO;
+    public SaborDTO toSaborDTO(Sabor saborEntidade){
+        return modelMapper.map(saborEntidade, SaborDTO.class);
     }
 
 
+    public SaborDTO cadastrar(SaborDTO saborDTO){
+        Assert.notNull(saborDTO.getIngredientes(),"Por favor, diga quais são os ingredientes!");
+        Assert.notNull(saborDTO.getNome(),"Por favor, digite um nome para o saborDTO!");
 
-    public SaborDTO cadastrar(SaborDTO sabor){
-        Assert.notNull(sabor.getIngredientes(),"Por favor, diga quais são os ingredientes!");
-        Assert.notNull(sabor.getNome(),"Por favor, digite um nome para o sabor!");
-//        OrderDTO orderDTO = modelMapper.map(order, OrderDTO.class);
 
-        return modelMapper.map(repository.save(toSaborEntidade(sabor)), SaborDTO.class);
+        return toSaborDTO(repository.save(toSabor(saborDTO)));
     }
+
 
 
     public SaborDTO update(SaborDTO sabor, long id) {
+        Assert.notNull(repository.findById(id).orElse(null),"Lamento, nenhum sabor localizado com esse ID.");
         Assert.notNull(sabor.getIngredientes(),"Por favor, diga quais são os ingredientes!");
         Assert.notNull(sabor.getNome(),"Por favor, digite um nome para o sabor!");
         Assert.notNull(sabor.getId(),"Por favor, insira um ID!");
 
-        return toSaborDTO(repository.save(toSaborEntidade(sabor)));
+        return modelMapper.map(repository.save(modelMapper.map(sabor, Sabor.class)),SaborDTO.class);
     }
 
     public void  delete(long id){
