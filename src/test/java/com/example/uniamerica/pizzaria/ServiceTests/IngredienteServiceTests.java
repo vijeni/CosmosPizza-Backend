3fd642bc4feed7efcf965720448d7a0e5e5d6cba
vainicius <comercial.viniciusgabriel@gmail.com>
2023-09-20 13:22:36 -0300

    test: Configurações iniciais

---
+++ src/test/java/com/example/uniamerica/pizzaria/ServiceTests/IngredienteServiceTests.java
@@ -0,0 +1,23 @@
+package com.example.uniamerica.pizzaria.ServiceTests;
+
+import com.example.uniamerica.pizzaria.Controller.IngredienteController;
+import com.example.uniamerica.pizzaria.Repository.IngredienteRepository;
+import com.example.uniamerica.pizzaria.Service.IngredientesService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IngredienteServiceTests {
+
+    @Mock
+    private IngredienteRepository ingredienteRepository;
+
+    @Mock
+    private IngredientesService ingredientesService;
+
+
+
+
+
+}
