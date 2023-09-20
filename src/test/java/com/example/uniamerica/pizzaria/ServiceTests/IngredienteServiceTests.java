21f839c45759de5c3688480764b6d046b9c2ce55
vainicius <comercial.viniciusgabriel@gmail.com>
2023-09-20 14:27:31 -0300

    test: ingredienteFindByIdTest passed.

--- src/test/java/com/example/uniamerica/pizzaria/ServiceTests/IngredienteServiceTests.java
+++ src/test/java/com/example/uniamerica/pizzaria/ServiceTests/IngredienteServiceTests.java
@@ -1,11 +1,21 @@
 package com.example.uniamerica.pizzaria.ServiceTests;
 
 import com.example.uniamerica.pizzaria.Controller.IngredienteController;
+import com.example.uniamerica.pizzaria.DTO.IngredienteDTO;
+import com.example.uniamerica.pizzaria.Entity.Ingrediente;
 import com.example.uniamerica.pizzaria.Repository.IngredienteRepository;
 import com.example.uniamerica.pizzaria.Service.IngredientesService;
+import jakarta.persistence.Table;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 public class IngredienteServiceTests {
@@ -13,9 +23,33 @@ public class IngredienteServiceTests {
     @Mock
     private IngredienteRepository ingredienteRepository;
 
-    @Mock
+    @InjectMocks
     private IngredientesService ingredientesService;
 
+    private IngredienteDTO ingredienteDTO = new IngredienteDTO();
+    private Ingrediente ingrediente = new Ingrediente();
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+
+    }
+
+    @Test
+    void ingredienteFindByIdTest(){
+        Mockito.when(ingredienteRepository.findById(1L)).thenReturn(Optional.of(ingrediente));
+
+        IngredienteDTO result = ingredientesService.findByID(1L);
+
+        Assertions.assertNotNull(result);
+
+        Mockito.verify(ingredienteRepository, Mockito.times(1)).findById(1L);
+    }
+
+    @Test
+    void ingredientePostTest(){
+
+    }
 
 
 
