c0adefb67263568857af4f3a9086b55ecd2800c3
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-09-21 13:37:25 -0300

    test: sonarqube main test

--- src/test/java/com/example/uniamerica/pizzaria/PizzariaApplicationTests.java
+++ src/test/java/com/example/uniamerica/pizzaria/PizzariaApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.uniamerica.pizzaria;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class PizzariaApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
+
 	}
 
 }
