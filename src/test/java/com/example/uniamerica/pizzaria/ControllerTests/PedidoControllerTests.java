0567ee3a467707e651228d5408521818553ad70f
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-09-21 19:38:54 -0300

    test: teste de integracao de pedido

--- src/test/java/com/example/uniamerica/pizzaria/ControllerTests/PedidoControllerTests.java
+++ src/test/java/com/example/uniamerica/pizzaria/ControllerTests/PedidoControllerTests.java
@@ -1,100 +1,138 @@
 package com.example.uniamerica.pizzaria.ControllerTests;
 
 import com.example.uniamerica.pizzaria.controller.PedidoController;
-import com.example.uniamerica.pizzaria.dto.PedidoDTO;
-import com.example.uniamerica.pizzaria.dto.PessoaDTO;
-import com.example.uniamerica.pizzaria.service.PedidoService;
+import com.example.uniamerica.pizzaria.dto.*;
+import com.example.uniamerica.pizzaria.entity.*;
+import com.example.uniamerica.pizzaria.repository.PedidoRepository;
+import com.example.uniamerica.pizzaria.repository.PessoaRepository;
+import com.example.uniamerica.pizzaria.service.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class PedidoControllerTests {
-    @InjectMocks
+    @Autowired
     private PedidoController controller;
-
-    @Mock
+    @Autowired
     private PedidoService service;
-
+    @MockBean
+    private PedidoRepository repository;
+    @MockBean
+    private PizzaService pizzaService;
+    @MockBean
+    private ProdutoService produtoService;
+    @MockBean
+    private PessoaRepository pessoaRepository;
+    PedidoDTO pedidoDTO = new PedidoDTO();
+    Pedido pedidoEntity = new Pedido();
+    List<PedidoDTO> pedidoDTOList = new ArrayList<>();
+    List<Pedido> pedidoEntityList = new ArrayList<>();
+    List<PizzaDTO> pizzasDTO = new ArrayList<>();
+    List<Pizza> pizzas = new ArrayList<>();
+    List<ProdutoDTO> produtoDTOS = new ArrayList<>();
+    List<Produto> produtos = new ArrayList<>();
+    Pessoa pessoa = new Pessoa();
+    PessoaDTO pessoaDTO = new PessoaDTO();
     @BeforeEach
     void setupMocks() {
         MockitoAnnotations.openMocks(this);
+        pessoa.setId(1L);
+        pessoaDTO.setId(1L);
+        pedidoDTO.setId(1L);
+        pedidoDTO.setStatus(Status.AGUARDANDO_CONFIRMACAO);
+        pedidoDTO.setValorPedido(10D);
+        pedidoDTO.setDataAbertura(LocalDateTime.now());
+        pedidoDTO.setEntrega(true);
+        pedidoDTO.setFormaPagamento(Pagamento.DEBITO);
+        pedidoDTO.setValorEntrega(5D);
+        pedidoDTO.setValorTotal(15D);
+        pedidoDTO.setDataAbertura(LocalDateTime.of(2023, Month.SEPTEMBER, 20, 0, 0));
+        pedidoDTO.setFuncionario(pessoaDTO);
+        pedidoDTO.setCliente(pessoaDTO);
+        pizzasDTO.add(new PizzaDTO());
+        produtoDTOS.add(new ProdutoDTO());
+        pedidoDTO.setPizzas(pizzasDTO);
+        pedidoDTO.setProdutos(produtoDTOS);
+        pedidoDTOList.add(pedidoDTO);
+
+        pedidoEntity.setId(1L);
+        pedidoEntity.setStatus(Status.AGUARDANDO_CONFIRMACAO);
+        pedidoEntity.setValorPedido(10D);
+        pedidoEntity.setEntrega(true);
+        pedidoEntity.setFormaPagamento(Pagamento.DEBITO);
+        pedidoEntity.setValorEntrega(5D);
+        pedidoEntity.setValorTotal(15D);
+        pedidoEntity.setDataAbertura(LocalDateTime.of(2023, Month.SEPTEMBER, 20, 0, 0));
+        pedidoEntity.setFuncionario(pessoa);
+        pedidoEntity.setCliente(pessoa);
+        pizzas.add(new Pizza());
+        produtos.add(new Produto());
+        pedidoEntity.setPizzas(pizzas);
+        pedidoEntity.setProdutos(produtos);
+        pedidoEntityList.add(pedidoEntity);
+
+        when(repository.findById(1L)).thenReturn(Optional.of(pedidoEntity));
+        when(pizzaService.findById(1L)).thenReturn(new PizzaDTO());
+        when(produtoService.findById(1L)).thenReturn(new ProdutoDTO());
+        when(pessoaRepository.findById(1L)).thenReturn(Optional.of(new Pessoa()));
+        when(repository.findAll()).thenReturn(pedidoEntityList);
+        when(repository.save(Mockito.any(Pedido.class))).thenReturn(pedidoEntity);
+
+
     }
 
     @Test
     void findByIdTest() {
-        Long idPedido = 1L;
-        PedidoDTO pedidosDTO = new PedidoDTO();
-        pedidosDTO.setId(idPedido);
-        when(service.findById(idPedido)).thenReturn(pedidosDTO);
-
-        ResponseEntity<PedidoDTO> controllerResponse = controller.findById(idPedido);
+        ResponseEntity<PedidoDTO> controllerResponse = controller.findById(1L);
         assertEquals(HttpStatus.OK, controllerResponse.getStatusCode());
-        assertEquals(pedidosDTO, controllerResponse.getBody());
-        System.out.println(controllerResponse.getBody());
+        assertThat(controllerResponse.getBody()).usingRecursiveComparison().isEqualTo(pedidoDTO);
     }
     @Test
     void getAllTest(){
-        PedidoDTO pedido = new PedidoDTO();
-        pedido.setId(1L);
-        pedido.setCliente(new PessoaDTO());
-        pedido.setFuncionario(new PessoaDTO());
-        List<PedidoDTO> pedidosDTOList = new ArrayList<>();
-        pedidosDTOList.add(pedido);
-        when(service.getAll()).thenReturn(pedidosDTOList);
         ResponseEntity<List<PedidoDTO>> controllerResponse = controller.getAll();
         assertEquals(HttpStatus.OK, controllerResponse.getStatusCode());
-        assertEquals(pedidosDTOList, controllerResponse.getBody());
-        System.out.println(controllerResponse.getBody());
+        assertThat(controllerResponse.getBody()).usingRecursiveComparison().isEqualTo(pedidoDTOList);
     }
     @Test
     void finalizarPedidoTest() {
-        Long idPedido = 1L;
-        PedidoDTO pedido = new PedidoDTO();
-        pedido.setId(idPedido);
-
-        when(service.finalizarPedido(1L)).thenReturn(pedido);
         ResponseEntity<PedidoDTO> controllerResponse = controller.finalizarPedido(1L);
         assertEquals(HttpStatus.OK, controllerResponse.getStatusCode());
-        assertEquals(pedido, controllerResponse.getBody());
-        System.out.println(controllerResponse.getBody());
+        assertThat(controllerResponse.getBody()).usingRecursiveComparison().isEqualTo(pedidoDTO);
     }
     @Test
     void cadastrarPedidoTest() {
-        PedidoDTO pedido = new PedidoDTO();
-        when(service.cadastrar(pedido)).thenReturn(pedido);
-        ResponseEntity<PedidoDTO> controllerResponse = controller.cadastrar(pedido);
+        ResponseEntity<PedidoDTO> controllerResponse = controller.cadastrar(pedidoDTO);
         assertEquals(HttpStatus.OK, controllerResponse.getStatusCode());
-        assertEquals(pedido, controllerResponse.getBody());
+        assertThat(controllerResponse.getBody()).usingRecursiveComparison().isEqualTo(pedidoDTO);
     }
     @Test
     void editarPedidoTest() {
-        Long idPedido = 1L;
-        PedidoDTO pedido = new PedidoDTO();
-        pedido.setId(idPedido);
-        when(service.editar(idPedido, pedido)).thenReturn(pedido);
-        ResponseEntity<PedidoDTO> controllerResponse = controller.editar(idPedido, pedido);
+        ResponseEntity<PedidoDTO> controllerResponse = controller.editar(1L, pedidoDTO);
         assertEquals(HttpStatus.OK, controllerResponse.getStatusCode());
-        assertEquals(pedido, controllerResponse.getBody());
+        assertThat(controllerResponse.getBody()).usingRecursiveComparison().isEqualTo(pedidoDTO);
     }
     @Test
     void deletarPedidoTest() {
-        Long idPedido = 1L;
-        ResponseEntity<String> controllerResponse = controller.deletar(idPedido);
+        ResponseEntity<String> controllerResponse = controller.deletar(1L);
         assertEquals(HttpStatus.OK, controllerResponse.getStatusCode());
         assertEquals("Pedido com ID 1 foi deletado com sucesso!", controllerResponse.getBody());
-        System.out.println(controllerResponse.getBody());
     }
 
 }
