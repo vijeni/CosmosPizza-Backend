f576b4f8328a9597fb12510f4722476fb061cea9
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-09-21 13:05:02 -0300

    test: teste de integração pedido ok

--- src/test/java/com/example/uniamerica/pizzaria/ControllerTests/TamanhoControllerTests.java
+++ src/test/java/com/example/uniamerica/pizzaria/ControllerTests/TamanhoControllerTests.java
@@ -2,56 +2,72 @@ package com.example.uniamerica.pizzaria.ControllerTests;
 
 import com.example.uniamerica.pizzaria.Controller.TamanhoController;
 import com.example.uniamerica.pizzaria.DTO.TamanhoDTO;
+import com.example.uniamerica.pizzaria.Entity.Tamanho;
+import com.example.uniamerica.pizzaria.Repository.TamanhoRepository;
 import com.example.uniamerica.pizzaria.Service.TamanhoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class TamanhoControllerTests {
-    @InjectMocks
+    @Autowired
     private TamanhoController controller;
-    @Mock
+    @Autowired
     private TamanhoService service;
+    @MockBean
+    private TamanhoRepository repository;
+    private TamanhoDTO tamanhoDTO = new TamanhoDTO();
+    private Tamanho tamanhoEntity = new Tamanho();
+    List<TamanhoDTO> tamanhoDTOList = new ArrayList<>();
+    List<Tamanho> tamanhoEntityList = new ArrayList<>();
 
     @BeforeEach
     void setupMocks() {
+        tamanhoDTO.setId(1L);
+        tamanhoDTO.setTamanho("Média");
+        tamanhoDTO.setMaximoSabores(2);
+        tamanhoDTO.setValor(10D);
+        tamanhoDTOList.add(tamanhoDTO);
+
+        tamanhoEntity.setId(1L);
+        tamanhoEntity.setTamanho("Média");
+        tamanhoEntity.setMaximoSabores(2);
+        tamanhoEntity.setValor(10D);
+        tamanhoEntityList.add(tamanhoEntity);
+
         MockitoAnnotations.openMocks(this);
+        when(repository.findById(Mockito.anyLong())).thenReturn(Optional.ofNullable(tamanhoEntity));
+        when(repository.findAll()).thenReturn(tamanhoEntityList);
+
     }
 
     @Test
     void findByIdTest() {
-        Long idTamanho = 1L;
-        TamanhoDTO tamanho = new TamanhoDTO();
-        tamanho.setId(idTamanho);
-        when(service.findById(idTamanho)).thenReturn(tamanho);
-
-        ResponseEntity<TamanhoDTO> controllerResponse = controller.findById(idTamanho);
+        ResponseEntity<TamanhoDTO> controllerResponse = controller.findById(1L);
         assertEquals(HttpStatus.OK, controllerResponse.getStatusCode());
-        assertEquals(tamanho, controllerResponse.getBody());
+        assertThat(controllerResponse.getBody()).usingRecursiveComparison().isEqualTo(tamanhoDTO);
         System.out.println(controllerResponse.getBody());
     }
     @Test
     void getAllTest(){
-        TamanhoDTO tamanho = new TamanhoDTO();
-        tamanho.setId(1L);
-        List<TamanhoDTO> tamanhosList = new ArrayList<>();
-        tamanhosList.add(tamanho);
-        when(service.getAll()).thenReturn(tamanhosList);
         ResponseEntity<List<TamanhoDTO>> controllerResponse = controller.getAll();
         assertEquals(HttpStatus.OK, controllerResponse.getStatusCode());
-        assertEquals(tamanhosList, controllerResponse.getBody());
+        assertThat(controllerResponse.getBody()).usingRecursiveComparison().isEqualTo(tamanhoDTOList);
         System.out.println(controllerResponse.getBody());
     }
     @Test
