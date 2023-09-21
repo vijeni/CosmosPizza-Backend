f17e2584bd7cfae8d28235992272287e06e8aa68
vainicius <comercial.viniciusgabriel@gmail.com>
2023-09-21 00:21:10 -0300

    test: pessoaGetByIdTest passed

---
+++ src/test/java/com/example/uniamerica/pizzaria/ServiceTests/PessoaServiceTests.java
@@ -0,0 +1,75 @@
+package com.example.uniamerica.pizzaria.ServiceTests;
+
+import com.example.uniamerica.pizzaria.DTO.EnderecoDTO;
+import com.example.uniamerica.pizzaria.DTO.PessoaDTO;
+import com.example.uniamerica.pizzaria.Entity.Endereco;
+import com.example.uniamerica.pizzaria.Entity.Ingrediente;
+import com.example.uniamerica.pizzaria.Entity.Pessoa;
+import com.example.uniamerica.pizzaria.Entity.TipoPessoa;
+import com.example.uniamerica.pizzaria.Repository.PessoaRepository;
+import com.example.uniamerica.pizzaria.Service.PessoaService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class PessoaServiceTests {
+    @Mock
+    private PessoaRepository repository;
+
+    @InjectMocks
+    private PessoaService service;
+
+    private List<PessoaDTO> pessoaDTOList = new ArrayList<>();
+    private PessoaDTO pessoaDTO = new PessoaDTO();
+    private List<Endereco>enderecoList;
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+
+        Pessoa pessoaEntidade = new Pessoa();
+        pessoaEntidade.setTipoPessoa(TipoPessoa.FUNCIONARIO);
+        pessoaEntidade.setId(1L);
+        pessoaEntidade.setNome("nome");
+        pessoaEntidade.setCpf("cpf");
+        pessoaEntidade.setTelefone("telefone");
+
+        pessoaDTO.setTipoPessoa(TipoPessoa.FUNCIONARIO);
+        pessoaDTO.setCpf("cpf");
+        pessoaDTO.setId(2L);
+        pessoaDTO.setNome("nome");
+        pessoaDTO.setTelefone("telefone");
+
+        pessoaDTOList.add(pessoaDTO);
+
+
+        when(repository.findAll()).thenReturn(pessoaDTOList.stream().map(pessoaDTO -> service.toPessoa(pessoaDTO)).toList());
+        when(repository.save(Mockito.any(Pessoa.class))).thenReturn(pessoaEntidade);
+        when(repository.findById(1L)).thenReturn(Optional.of(pessoaEntidade));
+
+    }
+
+    @Test
+    void pessoaGetById(){
+        PessoaDTO result = service.findById(1L);
+        Assertions.assertNotNull(result);
+        verify(repository,times(1)).findById(1L);
+    }
+
+
+
+
+
+}
