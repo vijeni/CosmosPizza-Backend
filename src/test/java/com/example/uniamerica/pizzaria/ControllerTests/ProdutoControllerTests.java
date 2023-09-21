004d7c10401bc00669283a033e37fe8d827ba830
Jean - Autofoz <jeanfelipe0610@gmail.com>
2023-09-21 13:46:59 -0300

    test: sonarqube fixes

--- src/test/java/com/example/uniamerica/pizzaria/ControllerTests/ProdutoControllerTests.java
+++ src/test/java/com/example/uniamerica/pizzaria/ControllerTests/ProdutoControllerTests.java
@@ -2,58 +2,66 @@ package com.example.uniamerica.pizzaria.ControllerTests;
 
 import com.example.uniamerica.pizzaria.controller.ProdutoController;
 import com.example.uniamerica.pizzaria.dto.ProdutoDTO;
+import com.example.uniamerica.pizzaria.entity.Produto;
+import com.example.uniamerica.pizzaria.repository.ProdutoRepository;
 import com.example.uniamerica.pizzaria.service.ProdutoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ProdutoControllerTests {
-    @InjectMocks
+    @Autowired
     private ProdutoController controller;
 
-    @Mock
+    @Autowired
     private ProdutoService service;
+    @MockBean
+    private ProdutoRepository repository;
+    ProdutoDTO produtoDTO = new ProdutoDTO();
+    Produto produtoEntity = new Produto();
+    List<ProdutoDTO> produtoDTOList = new ArrayList<>();
+    List<Produto> produtoEntityList = new ArrayList<>();
+
+
 
     @BeforeEach
     void setupMocks() {
         MockitoAnnotations.openMocks(this);
+        produtoDTO.setId(1L);
+        produtoDTOList.add(produtoDTO);
+        produtoEntity.setId(1L);
+        produtoEntityList.add(produtoEntity);
+
+        when(repository.findById(1L)).thenReturn(Optional.ofNullable(produtoEntity));
+        when(repository.findAll()).thenReturn(produtoEntityList);
+
     }
 
     @Test
     void findByIdTest() {
-        Long idProduto = 1L;
-        ProdutoDTO produto = new ProdutoDTO();
-        produto.setId(idProduto);
-        when(service.findById(idProduto)).thenReturn(produto);
-
-        ResponseEntity<ProdutoDTO> controllerResponse = controller.findById(idProduto);
+        ResponseEntity<ProdutoDTO> controllerResponse = controller.findById(1L);
         assertEquals(HttpStatus.OK, controllerResponse.getStatusCode());
-        assertEquals(produto, controllerResponse.getBody());
-        System.out.println(controllerResponse.getBody());
+        assertThat(controllerResponse.getBody()).usingRecursiveComparison().isEqualTo(produtoEntity);
     }
     @Test
     void getAllTest(){
-        ProdutoDTO produto = new ProdutoDTO();
-        produto.setId(1L);
-        List<ProdutoDTO> produtosList = new ArrayList<>();
-        produtosList.add(produto);
-        when(service.getAll()).thenReturn(produtosList);
         ResponseEntity<List<ProdutoDTO>> controllerResponse = controller.getAll();
         assertEquals(HttpStatus.OK, controllerResponse.getStatusCode());
-        assertEquals(produtosList, controllerResponse.getBody());
-        System.out.println(controllerResponse.getBody());
+        assertThat(controllerResponse.getBody()).usingRecursiveComparison().isEqualTo(produtoDTOList);
     }
     @Test
     void getAllByNome(){
