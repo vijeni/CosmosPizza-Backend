bd8af36b10a9ffba4f14231104b8d7281589c677
vainicius <comercial.viniciusgabriel@gmail.com>
2023-11-11 18:02:24 -0300

    feat: SecurityConfig ok

--- src/main/java/com/example/uniamerica/pizzaria/configs/SecurityConfig.java
+++ src/main/java/com/example/uniamerica/pizzaria/configs/SecurityConfig.java
@@ -1,15 +1,45 @@
 package com.example.uniamerica.pizzaria.configs;
 
+import com.example.uniamerica.pizzaria.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private UsuarioService service;
 
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(service);
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .cors(cors -> cors.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login/**").permitAll()
+                        .anyRequest().authenticated());
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(c -> c.disable());
+        return http.build();
+    }
+
+    @Bean
+    static PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 }
