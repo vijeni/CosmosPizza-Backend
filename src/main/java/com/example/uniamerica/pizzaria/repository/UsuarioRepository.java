f32c7cca5e0da4cb19b43d2cbeb85c65bcc897f6
vainicius <comercial.viniciusgabriel@gmail.com>
2023-11-11 16:55:02 -0300

    feat: Criando Repository de Usuario

--- src/main/java/com/example/uniamerica/pizzaria/repository/UsuarioRepository.java
+++ src/main/java/com/example/uniamerica/pizzaria/repository/UsuarioRepository.java
@@ -2,16 +2,19 @@ package com.example.uniamerica.pizzaria.repository;
 
 import com.example.uniamerica.pizzaria.entity.Cliente;
 import com.example.uniamerica.pizzaria.entity.Role;
+import com.example.uniamerica.pizzaria.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+public interface UsuarioRepository extends JpaRepository<Long, Usuario> {
+    @Query("SELECT p from Usuario p where p.role = :role")
+    List<Cliente> findAllByTipo(@Param("role") Role role);
 
-    @Query("from Cliente where cpf = :cpf")
+    @Query("from Usuario where cpf = :cpf")
     public List<Cliente> findByCpf(@Param("cpf") final String cpf);
+
+
 }
