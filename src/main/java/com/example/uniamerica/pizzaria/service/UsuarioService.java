f75f633c4e48bf5f8fcb85f3a24ba6a3dd27c85b
vainicius <comercial.viniciusgabriel@gmail.com>
2023-11-11 17:58:12 -0300

    fix: Usuario Service implements UserDetailsService ok

--- src/main/java/com/example/uniamerica/pizzaria/service/UsuarioService.java
+++ src/main/java/com/example/uniamerica/pizzaria/service/UsuarioService.java
@@ -7,6 +7,7 @@ import com.example.uniamerica.pizzaria.repository.UsuarioRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,7 @@ import org.springframework.util.Assert;
 import java.util.List;
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService {
     @Autowired
    private UsuarioRepository repository;
 
