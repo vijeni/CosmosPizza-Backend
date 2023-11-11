926ad560468e8192d7b93a2de6b137499ace9da3
vainicius <comercial.viniciusgabriel@gmail.com>
2023-11-11 17:40:57 -0300

    feat: Consulta de username

--- src/main/java/com/example/uniamerica/pizzaria/repository/UsuarioRepository.java
+++ src/main/java/com/example/uniamerica/pizzaria/repository/UsuarioRepository.java
@@ -1,6 +1,5 @@
 package com.example.uniamerica.pizzaria.repository;
 
-import com.example.uniamerica.pizzaria.entity.Cliente;
 import com.example.uniamerica.pizzaria.entity.Role;
 import com.example.uniamerica.pizzaria.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,12 +8,14 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface UsuarioRepository extends JpaRepository<Long, Usuario> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     @Query("SELECT p from Usuario p where p.role = :role")
-    List<Cliente> findAllByTipo(@Param("role") Role role);
+    List<Usuario> findAllByTipo(@Param("role") Role role);
 
     @Query("from Usuario where cpf = :cpf")
-    public List<Cliente> findByCpf(@Param("cpf") final String cpf);
+    public List<Usuario> findByCpf(@Param("cpf") final String cpf);
 
+    @Query("from Usuario where username = :username")
+    public List<Usuario> findByUsername(@Param("username") final String username);
 
 }
